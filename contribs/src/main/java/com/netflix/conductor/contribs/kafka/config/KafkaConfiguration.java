7f2389daced2ffb5b5fc1c300a48c373ff4bd2a6
trapprakam <rakim@gliaecosystems.com>
2021-04-29 11:55:53 +0100

    Some work on refactoring

--- contribs/src/main/java/com/netflix/conductor/contribs/kafka/config/KafkaConfiguration.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/kafka/config/KafkaConfiguration.java
@@ -6,6 +6,7 @@ import com.netflix.conductor.contribs.kafka.resource.handlers.ResourcesLoader;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,13 +18,13 @@ public class KafkaConfiguration {
 
     @ConditionalOnMissingBean
     @Bean
-    public ResourceHandler resourceHandler(final ObjectMapper objectMapper){
-        return new ResourceHandler(objectMapper, resourcesLoader());
+    public ResourceHandler resourceHandler(final ObjectMapper objectMapper, final ApplicationContext applicationContext){
+        return new ResourceHandler(applicationContext, objectMapper, resourcesLoader(applicationContext));
     }
 
-    public ResourcesLoader resourcesLoader(){
+    public ResourcesLoader resourcesLoader(final ApplicationContext applicationContext){
         final String RESOURCE_PATH = "com.netflix.conductor.rest.controllers";
-        return new ResourcesLoader(RESOURCE_PATH);
+        return new ResourcesLoader(applicationContext, RESOURCE_PATH);
     }
 
 }
---
+++ contribs/src/main/java/com/netflix/conductor/contribs/kafka/model/RequestPayload.java
@@ -0,0 +1,124 @@
+package com.netflix.conductor.contribs.kafka.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestPayload {
+
+    private final String key;
+    private final String resourceURI;
+    private final String httpMethod;
+    private final Object entity;
+    // The below fields are used for exception handling in kafka streams during deserialization
+    private boolean deserializationErrorOccurred;
+    private String deserializationError;
+
+    public RequestPayload(final String key, final String resourceURI, final String httpMethod,
+                            final Object entity) {
+        this.key = key;
+        this.resourceURI = resourceURI;
+        this.httpMethod = httpMethod;
+        this.entity = entity;
+        this.deserializationErrorOccurred = false;
+        this.deserializationError = "";
+    }
+
+    /**
+     * Get the key provided with the request
+     *
+     * @return Key of request
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Get the requested URI
+     * @return URI
+     */
+    public String getResourceURI() {
+        return resourceURI;
+    }
+
+    /**
+     * Get the requested HTTP method
+     * @return HTTP method
+     */
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    /**
+     * Get the entity for the request
+     *
+     * @return Entity
+     */
+    public Object getEntity() {
+        return entity;
+    }
+
+    /**
+     * Get indication of if an error occurred during deserialization of request
+     *
+     * @return Indicator if an error occurred or not
+     */
+    public boolean isDeserializationErrorOccurred() {
+        return deserializationErrorOccurred;
+    }
+
+    /**
+     * Set indication of if an error occurred during deserialization of request
+     *
+     * @param deserializationErrorOccurred Indicator if an error occurred
+     */
+    public void setDeserializationErrorOccurred(boolean deserializationErrorOccurred) {
+        this.deserializationErrorOccurred = deserializationErrorOccurred;
+    }
+
+    /**
+     * Get error message that occurred during deserialization of request
+     *
+     * @return Error message of error that occurred during deserialization of request
+     */
+    public String getDeserializationError() {
+        return deserializationError;
+    }
+
+    /**
+     * Set error message that occurred during deserialization of request
+     *
+     * @param deserializationError Error message of error that occurred during deserialization of request
+     */
+    public void setDeserializationError(String deserializationError) {
+        this.deserializationError = deserializationError;
+    }
+
+    /**
+     * Creates a map containing the field values of Request Container
+     *
+     * @return Map of the field values of the class
+     */
+    public Map<String, Object> getRequestData() {
+        final Map<String, Object> requestData = new HashMap<>();
+        requestData.put("key", key);
+        requestData.put("resourceURI", resourceURI);
+        requestData.put("httpMethod", httpMethod);
+        requestData.put("entity", entity);
+        return requestData;
+    }
+
+    /**
+     * Creates a to String object for printing the requestPayload object
+     *
+     * @return String object of the class
+     */
+    @Override
+    public String toString() {
+        return "RequestPayload{" +
+                "key='" + key + '\'' +
+                ", resourceURI='" + resourceURI + '\'' +
+                ", httpMethod='" + httpMethod + '\'' +
+                ", entity=" + entity +
+                '}';
+    }
+}
